daa37398d7cc63c51148e653e2bc7d17d860fa71
Chryssa Karatza <chryssa@Orfeass-MacBook-Pro.local>
2024-03-08 16:20:06 +0000

    first commit

--- index.js
+++ index.js
@@ -14,11 +14,45 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+function isValidURL(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+app.use(bodyParser.json());
+
+// POST request handler
+app.post('/shorturl', (req, res) => {
+  const url = req.body.url;
+
+  app.get('/api/shorturl', function(req, res) {
+  
+    if (isValidURL(url)) {
+      res.json({
+        original_url: url,
+        short_url: 1
+      });
+    } else {
+      res.json({ error: 'invalid url' });
+    }
+  });
+
+  app.get('/api/shorturl/<short_url>', function(req, res) {
+
+  })
+
+  // Send a response back to the client
+  res.send('Data received successfully!');
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+// id: url_input
