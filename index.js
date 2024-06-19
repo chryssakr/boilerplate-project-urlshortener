70e91eaf4c7d9a350efc723ba1a33ce17cc632b7
Chryssa Karatza <chryssakaratza@gmail.com>
2024-06-19 16:36:28 +0100

    commit unstaged changes

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-//app.use(express.urlencoded({extended:true}))
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
